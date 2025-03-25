c3947d1ddc19f27fa0cde368fa725434d4ca3857
Ergül FERİK <140970403+ergulferik@users.noreply.github.com>
2025-03-25 16:58:14 +0300

    docs update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/laika-web/',
+  base: '/Laika-Web/',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
